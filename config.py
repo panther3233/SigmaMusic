dc024325b4cc910bcc8f3ec1ab7d87b6880ff4c3
—cleo <148713792+panther3233@users.noreply.github.com>
2024-03-07 04:42:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1274584757))
+OWNER_ID = int(getenv("OWNER_ID", 2010687946))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/panther3233/YourMusic",
+    "https://github.com/panther3233/SigmaMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/cleoosupport")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/cleoosupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SIGMAOFFICIALX")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SIGMAOFFICIALX")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
