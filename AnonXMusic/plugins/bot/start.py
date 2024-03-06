14259f40b6f87912ad2335eb1925c5720ca1f3c3
—cleo <148713792+panther3233@users.noreply.github.com>
2024-03-07 05:38:14 +0700

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -19,7 +19,7 @@ from AnonXMusic.utils.database import (
 )
 from AnonXMusic.utils.decorators.language import LanguageStart
 from AnonXMusic.utils.formatters import get_readable_time
-from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
+from AnonXMusic.utils.inline import help_pannel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
